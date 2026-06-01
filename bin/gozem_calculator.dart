8229a0482f9cde3024cbc75d96882fbb2162190a
eunicemeye18 <eunicemeye18@gmail.com>
2026-06-01 13:41:38 +0100

    premier commit

---
+++ bin/gozem_calculator.dart
@@ -0,0 +1,18 @@
+import 'package:gozem_calculator/gozem_calculator.dart' as gozem_calculator;
+
+void main(List<String> arguments) {
+  print('Hello world: ${gozem_calculator.calculate()}!');
+}
+String nomClient = 'Koffi';
+String client = 3;
+
+int nombreCourses = 0;
+int nombresCourses = 'quatre';
+
+double distanceKm = 3.7;
+double distancesKm = "trois virgule sept";
+
+bool estHeureDePointe = false;
+bool estHeureDeDepart = "true";
+
+var tauxBase = 250.0;
