d71f5c9d5900ec085ad1301b678258446049eec8
eunicemeye18 <eunicemeye18@gmail.com>
2026-06-02 15:25:01 +0100

    2eme commit

--- bin/gozem_calculator.dart
+++ bin/gozem_calculator.dart
@@ -3,16 +3,22 @@ import 'package:gozem_calculator/gozem_calculator.dart' as gozem_calculator;
 void main(List<String> arguments) {
   print('Hello world: ${gozem_calculator.calculate()}!');
 }
-String nomClient = 'Koffi';
-String client = 3;
+// String nomClient = 'Koffi';
+// String client = 3;
 
-int nombreCourses = 0;
-int nombresCourses = 'quatre';
+// int nombreCourses = 0;
+// int nombresCourses = 'quatre';
 
-double distanceKm = 3.7;
-double distancesKm = "trois virgule sept";
+// double distanceKm = 3.7;
+// double distancesKm = "trois virgule sept";
 
-bool estHeureDePointe = false;
-bool estHeureDeDepart = "true";
+// bool estHeureDePointe = false;
+// bool estHeureDeDepart = "true";
 
-var tauxBase = 250.0;
+// var tauxBase = 250.0;
+
+final String nomClient ='Koffi'; //le nom du client n'est pas connu d'avance parce que chaque client mettra son nom
+final int nombreCourses = 0; //le nombre de course n'est pas connue d'avance
+final double distanceKm = 4.2; //la distance également peut changer selon chaque déplacement
+
+const int prixParKm = 100; //le prix d'un km ne changera pas donc c'est une constante
