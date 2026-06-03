e953d8131d88aab8a44ef0c90fc87b0a22a04643
eunicemeye18 <eunicemeye18@gmail.com>
2026-06-03 11:48:11 +0100

    3eme commit

--- bin/gozem_calculator.dart
+++ bin/gozem_calculator.dart
@@ -1,8 +1,30 @@
-import 'package:gozem_calculator/gozem_calculator.dart' as gozem_calculator;
+// import 'package:gozem_calculator/gozem_calculator.dart' as gozem_calculator;
 
-void main(List<String> arguments) {
-  print('Hello world: ${gozem_calculator.calculate()}!');
+
+//Livrable Mercredi
+
+double calculerPrixCourse(double distanceKm, int prixParKm) {
+  return distanceKm * prixParKm;
 }
+
+void main() {
+  final String nomClient = "Jean";
+  final int nombreCourses = 3;
+  final double distanceKm = 12.5;
+
+  const int prixParKm = 100;
+
+  double prixCourse = calculerPrixCourse(distanceKm, prixParKm);
+  double prixTotal = prixCourse * nombreCourses;
+
+  print("Client : $nomClient");
+  print("Prix d'une course : $prixCourse FCFA");
+  print("Prix total pour $nombreCourses courses : $prixTotal FCFA");
+}
+
+
+//Livrable Lundi
+
 // String nomClient = 'Koffi';
 // String client = 3;
 
@@ -17,8 +39,10 @@ void main(List<String> arguments) {
 
 // var tauxBase = 250.0;
 
-final String nomClient ='Koffi'; //le nom du client n'est pas connu d'avance parce que chaque client mettra son nom
-final int nombreCourses = 0; //le nombre de course n'est pas connue d'avance
-final double distanceKm = 4.2; //la distance également peut changer selon chaque déplacement
 
-const int prixParKm = 100; //le prix d'un km ne changera pas donc c'est une constante
+//Livrable Mardi
+// final String nomClient ='Koffi'; //le nom du client n'est pas connu d'avance parce que chaque client mettra son nom
+// final int nombreCourses = 0; //le nombre de course n'est pas connue d'avance
+// final double distanceKm = 4.2; //la distance également peut changer selon chaque déplacement
+
+// const int prixParKm = 100; //le prix d'un km ne changera pas donc c'est une constante
